af75db0f2f33f20c79795822cc53b41f9cc9935f
Ryan Nkansah <ryannkan@gmail.com>
2023-12-24 11:14:16 -0500

    done

---
+++ dist/main.js
@@ -0,0 +1,26 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+function Node (v = null, next = null) {
+    return {
+        v: null,
+        next: null
+    }
+}
+
+function LinkedList() {
+    return {
+        head: null
+        
+    
+    
+    
+        }
+}
+
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTs7QUFFQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9saW5rZWQtbGlzdHMtanMvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiZnVuY3Rpb24gTm9kZSAodiA9IG51bGwsIG5leHQgPSBudWxsKSB7XG4gICAgcmV0dXJuIHtcbiAgICAgICAgdjogbnVsbCxcbiAgICAgICAgbmV4dDogbnVsbFxuICAgIH1cbn1cblxuZnVuY3Rpb24gTGlua2VkTGlzdCgpIHtcbiAgICByZXR1cm4ge1xuICAgICAgICBoZWFkOiBudWxsXG4gICAgICAgIFxuICAgIFxuICAgIFxuICAgIFxuICAgICAgICB9XG59XG5cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
---
+++ src/index.js
@@ -0,0 +1,17 @@
+function Node (v = null, next = null) {
+    return {
+        v: null,
+        next: null
+    }
+}
+
+function LinkedList() {
+    return {
+        head: null
+        
+    
+    
+    
+        }
+}
+
